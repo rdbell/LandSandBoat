1371275c57f1805540e880be338dce19b0466d19
developer <developer@local>
2026-07-19 11:15:34 -0600

    Test Armor Storage update

--- scripts/tests/systems/armorstorage.lua
+++ scripts/tests/systems/armorstorage.lua
@@ -18,3 +18,17 @@ describe('Armor Storage trigger', function()
         assert(event[5] == 1 and event[6] == 1234 and event[7] == 0)
     end)
 end)
+
+describe('Armor Storage update', function()
+    it('updates the withdrawal menu with the selected set details', function()
+        local event = nil
+        local player = {
+            updateEvent = function(_, ...) event = { ... } end,
+        }
+
+        xi.armorStorage.onEventUpdate(player, 900, 1, 900)
+
+        assert(event[1] == 12511 and event[2] == 12638 and event[3] == 13961)
+        assert(event[4] == 14214 and event[5] == 14089 and event[6] == 500)
+    end)
+end)
