8835c167b23201a6f039afca454f525843e2ecb6
developer <developer@local>
2026-07-18 20:49:09 -0600

    Test Spiritreaver Poison priority

---
+++ src/map/ai/controllers/automaton_controller_spiritreaver_poison_priority_capacity.h
@@ -0,0 +1,9 @@
+#pragma once
+
+namespace automatoncontrollerspiritreaverpoison
+{
+inline auto CanPrioritize(uint8 waterManeuvers) -> bool
+{
+    return waterManeuvers >= 2;
+}
+} // namespace automatoncontrollerspiritreaverpoison
