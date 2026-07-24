82a63b2ccdd00c02fb02791ea4302873b8901123
developer <developer@local>
2026-07-24 08:40:46 -0600

    Document TRADE_REQ concrete Handler residual (6450)
    Pin Go packetsystem NewTradeReqHandler Validate/Process host surface.

--- src/map/packets/c2s/0x032_trade_req.cpp
+++ src/map/packets/c2s/0x032_trade_req.cpp
@@ -31,6 +31,8 @@
 #include "utils/charutils.h"
 #include "utils/jailutils.h"
 
+// Go host pure half: packetsystem.ValidateTradeReq / ProcessTradeReq /
+// NewTradeReqHandler (6450); policy mappacket.ClientTradeReqPlanFor.
 auto GP_CLI_COMMAND_TRADE_REQ::validate(MapSession* PSession, const CCharEntity* PChar) const -> PacketValidationResult
 {
     return PacketValidator(PChar)
