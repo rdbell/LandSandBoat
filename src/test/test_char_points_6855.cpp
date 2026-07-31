9043b442dbd777e7914f5c93cd2b65b96b6a1ca4
developer <developer@local>
2026-07-31 02:23:45 -0600

    Strengthen character point clamp tests

--- src/test/test_char_points_6855.cpp
+++ src/test/test_char_points_6855.cpp
@@ -13,6 +13,7 @@ auto runCharPoints6855SelfTests() -> bool
     ok = charpointshelpers::ClampPointTotal(3, -10, 100) == 0 && ok;
     ok = charpointshelpers::ClampPointTotal(90, 20, 100) == 100 && ok;
     ok = charpointshelpers::ClampPointTotal(std::numeric_limits<int32_t>::max(), 1, std::numeric_limits<int32_t>::max()) == std::numeric_limits<int32_t>::max() && ok;
+    ok = charpointshelpers::ClampPointTotal(0, std::numeric_limits<int32_t>::min(), std::numeric_limits<int32_t>::max()) == 0 && ok;
     ok = charpointshelpers::ClampPointTotal(20, 10, 0) == 0 && ok;
     ok = !charpointshelpers::ShouldAwardUnityAccolades(false, 10) && ok;
     ok = !charpointshelpers::ShouldAwardUnityAccolades(true, 0) && ok;
