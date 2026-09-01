56c81e2f2a4d961c741e349bb1c689ad6b513fa3
developer <developer@local>
2026-09-01 16:57:25 -0600

    Revert "Add death state virtual policy characterization"
    This reverts commit 9fc5400917243cf7091e51b2c7c7024857b51947.

--- src/map/ai/states/death_state.h
+++ src/map/ai/states/death_state.h
@@ -23,7 +23,6 @@
 #define _CDEATH_STATE_H
 
 #include "state.h"
-#include "death_state_policy.h"
 
 class CDeathState : public CState
 {
@@ -40,17 +39,17 @@ public:
     // whether the state can be changed by normal means
     virtual bool CanChangeState() override
     {
-        return deathstatepolicy::canChangeState();
+        return false;
     }
 
     virtual bool CanFollowPath() override
     {
-        return deathstatepolicy::canFollowPath();
+        return false;
     }
 
     virtual bool CanInterrupt() override
     {
-        return deathstatepolicy::canInterrupt();
+        return false;
     }
 
     void allowSendRaise();
